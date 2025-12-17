c5e9eaee2c630fba06db45ac51aecc376730f3d3
shahin <sarker4292@gmail.com>
2025-12-17 20:44:49 +0600

    update code

---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,18 @@
+import 'package:get/get.dart';
+
+class Dimensions {
+
+  static const double paddingSizeExtraSmall = 5.0;
+  static const double paddingSizeSimpleSmall = 8.0;
+  static const double paddingSizeSmall = 10.0;
+  static const double paddingSizeMedium = 12.0;
+  static const double paddingSizeDefault = 15.0;
+  static const double paddingSizeLarge = 20.0;
+  static const double paddingSizeExtraLarge = 24.0;
+
+  static const double radiusSmall = 5.0;
+  static const double radiusDefault = 10.0;
+  static const double radiusMedium = 15.0;
+  static const double radiusLarge = 20.0;
+  static const double radiusExtraLarge = 30.0;
+}
---
+++ lib/utils/styles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const interRegular = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w400,
+);
+
+const interMedium = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w500,
+);
+const interSemiBold = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w600,
+);
+
+const interBold = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w700,
+);
+
+const interBlack = TextStyle(
+  fontFamily: 'Inter',
+  fontWeight: FontWeight.w900,
+);
