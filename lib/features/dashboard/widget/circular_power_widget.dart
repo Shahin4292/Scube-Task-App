48630c87f1f10a606de043aa201916442d70f8b9
shahin <sarker4292@gmail.com>
2025-12-18 00:10:06 +0600

    added this widget

---
+++ lib/features/dashboard/widget/circular_power_widget.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:scube_task_app/utils/app_color.dart';
+import 'package:scube_task_app/utils/dimensions.dart';
+import 'package:scube_task_app/utils/styles.dart';
+
+class CircularPowerWidget extends StatelessWidget {
+  final String title;
+  final String powerText;
+  final double size;
+  final double strokeWidth;
+
+  const CircularPowerWidget({
+    super.key,
+    required this.title,
+    required this.powerText,
+    this.size = 150,
+    this.strokeWidth = 20,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Container(
+          width: size,
+          height: size,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: AppColor.primary,
+              width: strokeWidth,
+            ),
+          ),
+        ),
+        Column(
+          mainAxisSize: .min,
+          children: [
+
+            Text(
+              title,
+              style: interRegular.copyWith(
+                fontSize: 12, color: AppColor.dukeBlue
+              ),
+            ),
+
+            const SizedBox(height: Dimensions.paddingSizeExtraSmall),
+
+            Text(
+              powerText,
+              style: interSemiBold.copyWith(
+                fontSize: 16, color: AppColor.dukeBlue
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
