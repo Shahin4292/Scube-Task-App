e2ca078252ec26a78c4871e27acdc17bb50b354d
shahin <sarker4292@gmail.com>
2025-12-17 20:01:28 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'features/login/screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +13,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
+      home: LoginScreen(),
     );
   }
 }
